c7f79a693ffc7149db7bcadbfe20f0a742ebca9a
MATHEUS GADELHA ALVES HERZOG <202208386271@alunos.ibmec.edu.br>
2024-03-06 08:49:53 -0300

    primeiro commit

---
+++ src/test/java/br/com/ibmec/cloud/Spotify2/Spotify2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.cloud.Spotify2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spotify2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
